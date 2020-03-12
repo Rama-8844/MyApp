c9102f7e2e8492771ec24b5c360bbfefa345ae87
Rama-8844 <38398244+Rama-8844@users.noreply.github.com>
2020-03-12 19:08:26 +0530

    SPFx Required Files
    SPFx Required Files

---
+++ ReactSpFxPnP.module.scss.ts
@@ -0,0 +1,21 @@
+/* tslint:disable */
+require("./ReactSpFxPnP.module.css");
+const styles = {
+  reactSpFxPnP: 'reactSpFxPnP_b997b43b',
+  container: 'container_b997b43b',
+  row: 'row_b997b43b',
+  column: 'column_b997b43b',
+  'ms-Grid': 'ms-Grid_b997b43b',
+  fontRed: 'fontRed_b997b43b',
+  title: 'title_b997b43b',
+  subTitle: 'subTitle_b997b43b',
+  description: 'description_b997b43b',
+  hideElement: 'hideElement_b997b43b',
+  button: 'button_b997b43b',
+  label: 'label_b997b43b',
+  hideElementManager: 'hideElementManager_b997b43b',
+  customFont: 'customFont_b997b43b'
+};
+
+export default styles;
+/* tslint:enable */
